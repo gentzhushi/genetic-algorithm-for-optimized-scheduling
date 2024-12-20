c2552f96dd4c915cd06551fb16681682fdf997d2
Euronii <99759785+Euronii@users.noreply.github.com>
2024-12-20 20:34:14 +0100

    Recapi
    Perdorimi i JSON si data
    
    Co-Authored-By: Gent Zhushi <132105731+gentzhushi@users.noreply.github.com>

--- main.py
+++ main.py
@@ -6,9 +6,9 @@
             aka machineName -> [task name, task duration]
         2 - golla osht me minimizu total duration (fitness functiuon osht total duration, minimalet tu u zgjedh)
         3 - funksionon nbaz t3 funksioneve: 
-            crossover - i bon crossover 2 genotypes
-            mutation - e merr ni gjen, edhe ja shton ni curveball
-            selection - i zgjedh top fittest genes prej ni popullation
+             > crossover - i bon crossover 2 genotypes
+             > mutation - e merr ni kromozom, edhe ja shton ni curveball (e ndrron ni gjen veq palidhje tu shpresu per ma tmiren)
+             > selection - i zgjedh top fittest genes prej ni popullation
         4 - duhet me keep track t gjeneratave t orareve sa t ekzekutohet algoritmi(me i grafu)
         
     params:
@@ -19,3 +19,6 @@
     returns:
         - [optimal schedule, prduction time]
 """
+
+
+
--- src.py
+++ src.py
@@ -1,34 +1,12 @@
 import random
+import json
 
-machines = {
-    'Lathe': [('Turning', 30), ('Milling', 25), ('Drilling', 20)],
-    'Milling Machine': [('Milling', 30), ('Drilling', 25), ('Cutting', 15), ('Trimming', 30)],
-    'Welder': [('Welding', 40), ('Cutting', 20), ('Grinding', 30)],
-    'Grinder': [('Grinding', 25), ('Polishing', 15), ('Trimming', 20)],
-    'Saw': [('Cutting', 40), ('Trimming', 35)],
-    'Bender': [('Bending', 30), ('Forming', 20), ('Cutting', 10)],
-    'Press': [('Pressing', 35), ('Forming', 25)],
-    'CNC Router': [('Cutting', 25), ('Milling', 20), ('Engraving', 15)],
-    'Sander': [('Sanding', 30), ('Polishing', 20)],
-    'Laser Cutter': [('Cutting', 30), ('Engraving', 20)]
-}
+with open("json/machines.json") as file:
+    machines = json.load(file)
 
-tasks = {
-    'Turning': ['Lathe'],
-    'Milling': ['Lathe', 'Milling Machine', 'CNC Router'],
-    'Drilling': ['Lathe', 'Milling Machine'],
-    'Welding': ['Welder'],
-    'Grinding': ['Welder', 'Grinder'],
-    'Polishing': ['Grinder', 'Sander'],
-    'Cutting': ['Milling Machine', 'Welder', 'Saw', 'Bender', 'CNC Router', 'Laser Cutter'],
-    'Trimming': ['Milling Machine', 'Saw', 'Grinder'],
-    'Bending': ['Bender'],
-    'Forming': ['Bender', 'Press'],
-    'Pressing': ['Press'],
-    'Engraving': ['CNC Router', 'Laser Cutter'],
-    'Sanding': ['Sander']
-}
-# turning milling drilling
+with open("json/tasks.json") as file:
+    tasks = json.load(file)
+
 def generate_chromosome(tasks_list: list) -> list:
     # ka me gjenerate random choice of machine for said task, for every task
     for task in tasks_list:
@@ -37,6 +15,7 @@ def generate_chromosome(tasks_list: list) -> list:
     return chromosome
 
 def calculate_fitness(chromosome: list): # ka me kalkulu total time tni kromozomit
+    
     return
 
 def selection(generation): # ka me i zgjedh gjysen ma fit tni gjenerates
