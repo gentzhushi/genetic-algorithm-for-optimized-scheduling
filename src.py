4e1cd34e56731f62ebfee1731f066c5b0d0f5f25
Gresa-Hasani <gresahasani16@gmail.com>
2024-12-20 16:03:23 +0100

    Setup machines dataset and algorithm functions for genetic algorithm

---
+++ src.py
@@ -0,0 +1,23 @@
+machines = { # dataseti, ka me u marr sikur input
+    "Polisher": [("Polishing", 30), ("Buffing", 20)],
+    "Cutter": [("Cutting", 40), ("Trimming", 25)],
+    "Engraver": [("Engraving", 35), ("Surface Prep", 15)],
+    "Painter": [("Painting", 50), ("Coating", 45)],
+    "Inspector": [("Inspecting", 30), ("Testing", 25)],
+    "Driller": [("Drilling", 30), ("Finishing", 20)],
+}
+
+def generate_chromosome(machines): # ka me gjenerate random permutacion t machines
+    return
+
+def calculate_fitness(chromosome): # ka me kalkulu total time tni kromozomit
+    return
+
+def selection(generation): # ka me i zgjedh gjysen ma fit tni gjenerates
+    return
+
+def crossover(parent1, parent2): # ka me i bo crossover 2 kromozome
+    return
+
+def mutate(chromosome): # ka me mutate ni kromozom qe e merr si input
+    return
