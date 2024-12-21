1d2b112865ad2749104f63e931f88b53e67e7817
Gent Zhushi <gentzhushi01@gmail.com>
2024-12-21 09:56:24 +0100

    tu e harrnu at this point

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-# qitu vjen krejt kodi ig
+import json
+import src as util
+import random
 
 """
     qka ka me ndodh qitu:
@@ -20,5 +22,45 @@
         - [optimal schedule, prduction time]
 """
 
+with open("json/machines.json") as file:
+    machines = json.load(file)
 
+with open("json/tasks.json") as file:
+    tasks = json.load(file)
 
+POPULLATION_SIZE = 5
+NUM_GENERATIONS = 100
+MUTATION_RATE = 0.1
+CROSSOVER_RATE = 0.8
+
+POPULLATION = [util.generate_chromosome(tasks, tasks) for _ in range(POPULLATION_SIZE)]
+BEST_SCHEDULE = None
+BEST_SCHEDULES = []
+BEST_SCHEDULE_TIME = float("inf")
+BEST_SCHEDULE_TIMES = []
+
+if __name__ == "__main__":
+    for generation in range(NUM_GENERATIONS):
+        fitnesses = [util.calculate_fitness(schedule, machines) for schedule in POPULLATION]
+        local_best_schedule_time = min(fitnesses)
+        local_best_schedule = POPULLATION[fitnesses.index(local_best_schedule_time)]
+        BEST_SCHEDULE_TIMES += [local_best_schedule_time]
+        BEST_SCHEDULES += [local_best_schedule]
+        if local_best_schedule_time < BEST_SCHEDULE_TIME:
+            BEST_SCHEDULE_TIME = local_best_schedule_time
+            BEST_SCHEDULE = local_best_schedule
+        print(f"Generation [{generation + 1}] - \tBest Schedule: [{local_best_schedule_time}]-[{local_best_schedule}]")
+        
+        new_popullation = []
+        top_fittest = util.selection(POPULLATION)
+        while len(new_popullation) < POPULLATION_SIZE:
+            parent1 = top_fittest.pop(0) # pop front
+            parent2 = top_fittest.pop(0)
+            child1, child2 = util.crossover(parent1, parent2, CROSSOVER_RATE)
+            new_popullation.append(util.mutate(child1, tasks))
+            if len(new_popullation) < POPULLATION_SIZE:
+                new_popullation.append(util.mutate(child2, tasks))
+        POPULLATION = new_popullation
+
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print(f"BEST_SCHEDULE_TIMES: {BEST_SCHEDULE_TIMES}")
--- src.py
+++ src.py
@@ -3,32 +3,22 @@ import json
 from collections import defaultdict
 import heapq
 
-with open("json/machines.json") as file:
-    machines = json.load(file)
-
-with open("json/tasks.json") as file:
-    tasks = json.load(file)
-
-
-def generate_chromosome(tasks_list: list) -> list:
+def generate_chromosome(tasks_list: list, tasks: dict) -> list:
     # ka me gjenerate random choice of machine for said task, for every task
     chromosome = defaultdict(list)
     for task in tasks_list:
         machine = random.choice(tasks[task])
-        chromosome[machine].append(task)
+        chromosome[task] = machine
     return chromosome
+    # task -> machine
 
-
-def calculate_fitness(chromosome: dict) -> int:
+def calculate_fitness(chromosome: dict, machines: dict) -> int:
     # kromozomi: machine -> task
     time_map = defaultdict(int)
-    for machine in chromosome.keys():
-        for task in chromosome[machine]:
+    for task, machine in chromosome.items():
             time_map[machine] += machines[machine][task]
-    return min(time_map.values())
-
-
-# ktheje kohen ma tgat per tcilen eshte ndonje maqine nxan.
+    return max(time_map.values())
+    # ktheje kohen ma tgat per tcilen eshte ndonje maqine nxan.
 
 def selection(generation: list) -> list:
     # ka me i zgjedh gjysen ma fit tni gjenerates (gjenerata osht array me kromozome)
@@ -37,56 +27,50 @@ def selection(generation: list) -> list:
     heapq.heapify(min_heap)
 
     # merri bottom 50%
-    selection = []
-    for i in range(len(generation) // 2):
-        selection.append(heapq.heappop(min_heap))
+    selection = [heapq.heappop(min_heap) for _ in range(len(generation) // 2)]
 
     return selection
 
-
-def crossover(parent1: dict, parent2: dict, CROSSOVER_RATE: float) -> tuple[
-    dict, dict]:  # ka me i bo crossover 2 kromozome
+def crossover(parent1: dict, parent2: dict, CROSSOVER_RATE: float) -> tuple[dict, dict]:
+    # ka me i bo crossover 2 kromozome
     if random.random() > CROSSOVER_RATE:
         return parent1, parent2
 
     child1 = {}
     child2 = {}
-    split_point = random.randint(1, len(parent1) - 2)
-
-    parent1keys = list(parent1.keys())
-    parent2keys = list(parent2.keys())
-
-    for i in range(split_point):
-        child1[parent1keys[i]] = parent1[parent1keys[i]]
-        child2[parent2keys[i]] = parent2[parent2keys[i]]
-
-    for i in range(split_point, len(parent1)):
-        child1[parent2keys[i]] = parent2[parent2keys[i]]
-        child2[parent1keys[i]] = parent1[parent1keys[i]]
-
+    for task in parent1.keys():
+        #FLIP A COIN A ME JA NDRRU MAQINEN PER TASKUN SPECIFIK A JO
+        #perndryshe kjo i bjen gjysen e seneve mi marr prej nanes gjysen prej babes, 
+        #amo, nuk i merr me ren po ne menyre heterogjene
+        if random.choice([True, False]): 
+            child1[task] = parent2[task]
+            child2[task] = parent1[task]
+        else:
+            child1[task] = parent1[task]
+            child2[task] = parent2[task]
     return child1, child2
 
-
-def mutate(chromosome: dict, MUTATION_RATE: float) -> dict:  # ka me mutate ni kromozom qe e merr si input
-    if random.random > MUTATION_RATE:
+def mutate(chromosome: dict, tasks: dict, MUTATION_RATE: float) -> dict:
+    # ka me mutate ni kromozom qe e merr si input
+    if random.random() > MUTATION_RATE:
         return chromosome
-
+    random_task = random.choice(chromosome.keys())
+    chromosome[random_task] = random.choice(tasks[random_task])
     return
 
+# # print(f"{machines}\n\n{tasks}")
+# chromosome1 = generate_chromosome(tasks)
+# chromosome2 = generate_chromosome(tasks)
 
-# print(f"{machines}\n\n{tasks}")
-chromosome1 = generate_chromosome(tasks)
-chromosome2 = generate_chromosome(tasks)
-
-print(f"kromozomi:\n{str(chromosome1)}")
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-
-print(f"fitness: \n{calculate_fitness(chromosome1)}")
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-
-# print(f"top 50%: {selection()}")
+# print(f"kromozomi:\n{str(chromosome1)}")
 # print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
-print(f"chromozomes: \n{str(chromosome1)}\n{str(chromosome2)}")
-print(f"crossover: \n{str(crossover(chromosome1, chromosome2, 1))}")
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+# print(f"fitness: \n{calculate_fitness(chromosome1)}")
+# print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+# # print(f"top 50%: {selection()}")
+# # print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+# print(f"chromozomes: \n{str(chromosome1)}\n{str(chromosome2)}")
+# print(f"crossover: \n{str(crossover(chromosome1, chromosome2, 1))}")
+# print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
